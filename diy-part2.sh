c1d79562bcbaaa36165c47fa3011c48e036dd627
zow2023 <125123077+zow2023@users.noreply.github.com>
2024-06-12 09:58:27 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-#
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 
-#
 
 git clone https://github.com/kiddin9/luci-app-dnsfilter package/luic-app-dnsfilter
 git clone https://github.com/sirpdboy/luci-app-parentcontrol package/luci-app-parentcontrol
@@ -34,7 +32,7 @@ git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 #git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 #git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 # Modify default IP
-sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate 
+sed -i 's/192.168.6.1/10.0.0.1/g' package/base-files/files/bin/config_generate 
 # Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
 #
 # This is free software, licensed under the MIT License.
