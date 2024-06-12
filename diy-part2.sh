d5009a7a7f6075e03955c901301c456d38f2dfde
zow2023 <125123077+zow2023@users.noreply.github.com>
2024-06-12 10:34:38 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -32,7 +32,7 @@ git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 #git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 #git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 # Modify default IP
-sed -i 's/192.168.6.1/10.0.0.1/g' package/base-files/files/bin/config_generate 
+#sed -i 's/192.168.6.1/10.0.0.1/g' package/base-files/files/bin/config_generate 
 # Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
 #
 # This is free software, licensed under the MIT License.
